9ac7b86eda9c3fb5850208c4139a7ceebafeb67d
Discoonect <67423146+Discoonect@users.noreply.github.com>
2020-08-04 17:35:18 +0900

    a

--- route/user.js
+++ route/user.js
@@ -6,7 +6,7 @@ const { createUser, loginUser, allLogout } = require("../control/user");
 const router = express.Router();
 
 router.route("/").post(createUser);
-router.route("/login").post(loginUser);
-router.route("/logout").post(allLogout);
+router.route("/login").post(auth, loginUser);
+router.route("/logout").post(auth, allLogout);
 
 module.exports = router;
